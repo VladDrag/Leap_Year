4c4e9d8a987ed901337afe2934eb3f5e608eaba5
vdrag <vdragomir@protonmail.com>
2022-02-13 16:24:49 +0100

    Leap year exercise

---
+++ leap_year.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "leap_year_utils.cpp"
+
+
+
+int main() {
+  std::string input;
+  int year;
+
+  while (1)
+  {
+	std::cout << "=============================================\n";
+    std::cout << "Please input your year:\n";
+	input = GetInput();
+	InputChecker(input);
+	year = GetYear(input);
+	if (YearChecker(year) == true)
+		LeapYearChecker(year);
+	else
+		continue;
+  }
+}
---
+++ leap_year_utils.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+std::string GetInput(){
+	std::string number;
+	if (!(std::cin >> number))
+		throw "Input could not be taken!";
+	return (number);
+}
+
+void InputChecker(std::string input)
+{
+	if (input.empty())
+		std::cout << "Please choose another year, larger than 999!";
+	else if (input == "exit")
+	{
+		std::cout <<"Thank you for checking our program. Closing program.\n";
+		exit(0);
+	}
+}
+
+int GetYear(std::string input)
+{
+	int year;
+	try {year = stoi(input);}
+	catch(const std::invalid_argument& ia) {
+		std::cerr << "<<<Invalid argument!>>>\n";
+		std::cerr << "Please input a valid year (i.e. 1999)\n";
+		return (-1);
+  	}
+	return (year);
+}
+
+bool YearChecker(int year)
+{
+	if (year == -1)
+		return (false);
+	if (year < 1000)
+    {
+      std::cout << "Please choose another year, larger than 999!\n";
+      return (false);
+    }
+	return (true);
+}
+
+void LeapYearChecker(int year)
+{
+	if (year % 4 == 0)
+	{
+		if ((year % 100 == 0) && (year % 400 != 0))
+			std::cout << "The year " << year << " is not a leap year.\n";
+		else if (year % 400 == 0)
+			std::cout << "The year " << year << " is a leap year.\n";
+	}
+	else
+		std::cout << "The year " << year << " is not leap year.\n";
+}
